567371e62edde3305d588310aea7ac55892eef32
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-07-10 21:25:20 +0100

    tweak git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,10 @@ then
 fi
 
 # The rest of my fun git aliases
+alias ga='git add'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glg=glog
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
@@ -16,4 +18,5 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='gs'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
