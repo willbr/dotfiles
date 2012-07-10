b4625ee478617505d47a7a1399ef6d29429c4b82
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-07-10 21:36:15 +0100

    add wjbr aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,26 @@
 alias reload!='. ~/.zshrc'
-alias v='vim'
+
+alias ll='ls -l'
+alias la='ls -a'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+
+alias l='ls'
+alias e='open .'
+alias q='exit'
+alias v='mvim -v'
+alias vim='mvim -v'
+alias vimrc='vim ~/.vimrc'
+
+alias t='todo'
+alias tl='do_this_next last'
+alias tel='do_this_next edit-last'
+alias n='do_this_next'
+alias ne='do_this_next edit'
+
+alias du='du -h'
+
+alias grep='grep --color=auto'
+. `jumpto alias`
 
