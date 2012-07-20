429dcdc4a10df6bbf4423bf3b97f7f490b4d7bc2
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-07-20 10:49:41 +0100

    fix zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,9 +72,10 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(git_dirty)$(need_push)\n› '
+
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[white]%}%1~%{$reset_color%}"
 }
 
 precmd() {
