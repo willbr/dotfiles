0772f838eb3ac01853b37d3e68cf5e3b496a107c
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-07-21 12:17:44 +0100

    tweak & add new git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,16 +7,29 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g='git'
+compdef g=git
 alias ga='git add'
+compdef _git ga=git-add
 alias gl='git pull --prune'
+compdef _git gl=git-pull
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias glg=glog
+compdef _git glog=git-log
 alias gp='git push origin HEAD'
+compdef _git gp=git-push
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
+alias gm='git merge'
+compdef _git gm=git-merge
 alias gco='git checkout'
+compdef _git gco=git-checkout
 alias gb='git branch'
+compdef _git gb=git-branch
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gst='gs'
+compdef _git gs=git-status
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
