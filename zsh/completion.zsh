24ab82c0f57ce1996db506ca1257d615a5d41d44
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-07-21 12:56:27 +0100

    tweak make completion easier

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,14 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+## case-insensitive (all),partial-word and then substring completion
+if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  unset CASE_SENSITIVE
+else
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+fi
 
+zstyle ':completion:*' list-colors ''
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+zstyle ':completion:*' menu select
+
